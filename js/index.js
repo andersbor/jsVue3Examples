2a114cc70a0e4a3564e0fa0c8a5d9b30d7398466
Anders Børjesson <anbo@zealand.dk>
2021-02-01 21:25:42 +0100

    examples

---
+++ js/index.js
@@ -0,0 +1,28 @@
+Vue.createApp({
+    data() {
+        return {
+            name: "Anders B",
+            teacher: { name: "Michael H", age: 29 },
+            weekdays: ["Monday", "Tuesday", "Wednesday"],
+            cars: [{ make: "Volvo", model: "Amazon" }, { make: "Audi", model: "A8" }, {make: "VW", model: "Polo"}],
+            counter: 0,
+            myNumber: 0,
+            inputMessage: "",
+            outputMessage: "",
+            checked: false,
+            anotherMessage: "Hello"
+        }
+    },
+    methods: {
+        reverse() { // side effects
+            this.outputMessage = this.inputMessage.split('').reverse().join('')
+        },
+        reverseProperFunction(message) { // proper function, no side effects
+            return message.split('').reverse().join('')
+        },
+        updateCounter() { // side effects
+            this.counter++
+        },
+        next(n) { return n + 1 }
+    }
+}).mount("#app")
